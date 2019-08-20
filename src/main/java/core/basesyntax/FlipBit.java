b31ecf106d0dee4bcbe85db8f017cd469569d66f
Max <skaterweather41@gmail.com>
2019-08-20 20:46:03 +0200

    solved in another way, using Bitwise operations

--- src/main/java/core/basesyntax/FlipBit.java
+++ src/main/java/core/basesyntax/FlipBit.java
@@ -8,16 +8,6 @@ public class FlipBit {
      * Договоримся, что биты нумеруются от младшего (индекс 1) к старшему (индекс 32).
      */
     public int flipBit(int value, int bitIndex) {
-        StringBuilder arr = new StringBuilder(Integer.toBinaryString(value));
-        if (bitIndex > arr.length()) {
-            arr = new StringBuilder();
-            for (int i = 0; i <= bitIndex - Integer.toBinaryString(value).length(); i++) {
-                arr.append('0');
-            }
-            arr.append(Integer.toBinaryString(value));
-        }
-        char correctBit = arr.charAt(arr.length() - bitIndex) == '1' ? '0' : '1';
-        arr.setCharAt(arr.length() - bitIndex, correctBit);
-        return Integer.parseInt(new String(arr), 2);
+        return value ^ 1 << bitIndex - 1;
     }
 }
