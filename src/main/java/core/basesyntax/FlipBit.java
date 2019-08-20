1e70d2945b80b006b25412d3b02b721393dcbeb8
kmety <kmety9876@gmail.com>
2019-08-20 15:26:06 +0300

    solution added

--- src/main/java/core/basesyntax/FlipBit.java
+++ src/main/java/core/basesyntax/FlipBit.java
@@ -1,15 +1,23 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+
 public class FlipBit {
 
     /**
      * Реализуйте метод flipBit(int value, int bitIndex), изменяющий значение одного бита
      * заданного целого числа на противоположное.
      * Данная задача актуальна, например, при работе с битовыми полями.
-     *
      * Договоримся, что биты нумеруются от младшего (индекс 1) к старшему (индекс 32).
      */
     public int flipBit(int value, int bitIndex) {
-        return 0;
+        String stringValue = String.format("%32s", Integer.toBinaryString(value)).replace(' ', '0');
+        StringBuilder stringBuilder = new StringBuilder(stringValue);
+        stringBuilder.reverse();
+        char[] array = stringBuilder.toString().toCharArray();
+        array[bitIndex - 1] = array[bitIndex - 1] == '0' ? '1' : '0';
+        stringBuilder = new StringBuilder(new String(array));
+        String result = stringBuilder.reverse().toString();
+        return Integer.valueOf(result, 2);
     }
 }
