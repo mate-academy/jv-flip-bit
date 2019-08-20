5223afb98a4a129aec610a450fcab6014728a43e
NikolayKatsai <alaska.rok123@gmail.com>
2019-08-20 14:40:02 +0300

    flipBit method changed in FlipBit class, now it uses bitwise operations

--- src/main/java/core/basesyntax/FlipBit.java
+++ src/main/java/core/basesyntax/FlipBit.java
@@ -9,15 +9,6 @@ public class FlipBit {
      * Договоримся, что биты нумеруются от младшего (индекс 1) к старшему (индекс 32).
      */
     public int flipBit(int value, int bitIndex) {
-        int valCoppy = value;
-        for (int i = 1; i < bitIndex; i++) {
-            valCoppy /= 2;
-        }
-        if (valCoppy % 2 == 1) {
-            value -= (int) Math.pow(2, bitIndex - 1);
-        } else {
-            value += (int) Math.pow(2, bitIndex - 1);
-        }
-        return value;
+        return value ^ (int) Math.pow(2, bitIndex - 1);
     }
 }
