23ba89aab873f5447f1e8472fe521e663b80d690
NikolayKatsai <alaska.rok123@gmail.com>
2019-08-20 13:31:30 +0300

    flipBit method finished in FlipBit class

--- src/main/java/core/basesyntax/FlipBit.java
+++ src/main/java/core/basesyntax/FlipBit.java
@@ -6,10 +6,18 @@ public class FlipBit {
      * Реализуйте метод flipBit(int value, int bitIndex), изменяющий значение одного бита
      * заданного целого числа на противоположное.
      * Данная задача актуальна, например, при работе с битовыми полями.
-     *
      * Договоримся, что биты нумеруются от младшего (индекс 1) к старшему (индекс 32).
      */
     public int flipBit(int value, int bitIndex) {
-        return 0;
+        int valCoppy = value;
+        for (int i = 1; i < bitIndex; i++) {
+            valCoppy /= 2;
+        }
+        if (valCoppy % 2 == 1) {
+            value -= (int) Math.pow(2, bitIndex - 1);
+        } else {
+            value += (int) Math.pow(2, bitIndex - 1);
+        }
+        return value;
     }
 }
