f5641dc43178cc1ee0001d7ff1399f2f6fbe266e
AndreyDMA <andreydmitriev@ukr.net>
2019-08-21 11:50:19 +0300

    second try solution simplified

--- src/main/java/core/basesyntax/FlipBit.java
+++ src/main/java/core/basesyntax/FlipBit.java
@@ -9,32 +9,22 @@ public class FlipBit {
      * Договоримся, что биты нумеруются от младшего (индекс 1) к старшему (индекс 32).
      */
     public int flipBit(int value, int bitIndex) {
-        String binaryNumber = "";
+        String binaryNumber = Integer.toBinaryString(value);
         int number = 0;
-        if (value == 0) {
-            binaryNumber = "0";
-        }
-        while (value >= 1) {
-            if (value % 2 != 0) {
-                binaryNumber += "1";
-            } else {
-                binaryNumber += "0";
-            }
-            value /= 2;
-        }
         if (binaryNumber.length() < bitIndex) {
             for (int i = 0; i < bitIndex; i++) {
-                binaryNumber += "0";
+                binaryNumber = "0" + binaryNumber;
             }
         }
         char[] array = binaryNumber.toCharArray();
-        if (array[bitIndex - 1] == '1') {
-            array[bitIndex - 1] = '0';
+        if (array[array.length - bitIndex] == '1') {
+            array[array.length - bitIndex] = '0';
         } else {
-            array[bitIndex - 1] = '1';
+            array[array.length - bitIndex] = '1';
         }
         for (int i = 0; i < array.length; i++) {
-            number += Character.getNumericValue(array[i]) * Math.pow(2, i);
+            number += Character.getNumericValue(array[i])
+                    * Math.pow(2, (array.length - 1 - i));
         }
         return number;
     }
