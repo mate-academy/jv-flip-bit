1af77fb050876dcca77dec837eb02bac482a4c4e
Taras Khalak <tarasulo@gmail.com>
2019-08-21 19:26:09 +0300

    'fixed'

--- src/main/java/core/basesyntax/FlipBit.java
+++ src/main/java/core/basesyntax/FlipBit.java
@@ -10,6 +10,7 @@ public class FlipBit {
      * Договоримся, что биты нумеруются от младшего (индекс 1) к старшему (индекс 32).
      */
     public int flipBit(int value, int bitIndex) {
+
         return value ^ (1 << bitIndex - 1);
     }
 }
