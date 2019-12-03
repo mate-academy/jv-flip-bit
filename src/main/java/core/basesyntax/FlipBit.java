e794fc8c12cbb06317006338359c83cea2374a91
Mykhailo Kramar <3805033@gmail.com>
2019-12-03 20:08:37 +0200

    hw 2.1

--- src/main/java/core/basesyntax/FlipBit.java
+++ src/main/java/core/basesyntax/FlipBit.java
@@ -9,6 +9,17 @@ public class FlipBit {
      * Договоримся, что биты нумеруются от младшего (индекс 1) к старшему (индекс 32).</p>
      */
     public int flipBit(int value, int bitIndex) {
-        return 0;
+        String binString = Integer.toBinaryString(value);
+        int binLength = binString.length();
+        if (binLength < bitIndex) {
+            for (int i = 0; i < bitIndex - binLength; i++) {
+                binString = '0' + binString;
+            }
+        }
+        char[] binChar = binString.toCharArray();
+        int n = binChar.length - bitIndex;
+        binChar[n] = binChar[n] == '0' ? '1' : '0';
+        String result = new String(binChar);
+        return Integer.parseInt(result, 2);
     }
 }
