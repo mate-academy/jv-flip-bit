6088a338065756bacbd99793a543e58540e9783c
Yaroslava Bondarchuk <yaroslava7bondarchuk@gmail.com>
2019-12-03 17:33:38 +0200

    added solution to flipBit method

--- src/main/java/core/basesyntax/FlipBit.java
+++ src/main/java/core/basesyntax/FlipBit.java
@@ -8,7 +8,25 @@ public class FlipBit {
      * Данная задача актуальна, например, при работе с битовыми полями.
      * Договоримся, что биты нумеруются от младшего (индекс 1) к старшему (индекс 32).</p>
      */
-    public int flipBit(int value, int bitIndex) {
-        return 0;
+    public static int flipBit(int value, int bitIndex) {
+        String binary = Integer.toBinaryString(value);
+        StringBuilder buildBinary = new StringBuilder();
+
+        if (bitIndex > binary.length()) {
+            for (int i = 0; i < bitIndex - binary.length(); i++) {
+                buildBinary.append("0");
+            }
+        }
+        buildBinary.append(binary);
+
+        if (buildBinary.charAt(buildBinary.length() - bitIndex) == '0') {
+            buildBinary.replace(buildBinary.length() - bitIndex,
+                    buildBinary.length() - bitIndex + 1, "1");
+        } else {
+            buildBinary.replace(buildBinary.length() - bitIndex,
+                    buildBinary.length() - bitIndex + 1, "0");
+        }
+
+        return Integer.parseInt(buildBinary.toString(), 2);
     }
 }
