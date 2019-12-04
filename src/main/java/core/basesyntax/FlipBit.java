da1eca2c7ff05d29a3aba4721a04064e3658d126
Vadim Chaika <proxik777@gmail.com>
2019-12-04 17:46:02 +0200

    added a solution for flipbit homework

--- src/main/java/core/basesyntax/FlipBit.java
+++ src/main/java/core/basesyntax/FlipBit.java
@@ -9,6 +9,17 @@ public class FlipBit {
      * Договоримся, что биты нумеруются от младшего (индекс 1) к старшему (индекс 32).</p>
      */
     public int flipBit(int value, int bitIndex) {
-        return 0;
+        StringBuilder bin = new StringBuilder(Integer.toBinaryString(value));
+        StringBuilder result = new StringBuilder();
+        int stringLength = bin.length();
+        if (stringLength < bitIndex) {
+            for (int i = 0; i < bitIndex - stringLength; i++) {
+                result.append('0');
+            }
+        }
+        result.append(bin);
+        char bit = (result.charAt(result.length() - bitIndex) == '0') ? '1' : '0';
+        result.setCharAt(result.length() - bitIndex, bit);
+        return Integer.parseInt(result.toString(), 2);
     }
 }
