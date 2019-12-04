9d97b90b0c3f04816c367c399953b200b15740c3
Taraban <Taraban.dmitry@gmail.com>
2019-12-04 12:24:46 +0200

    added hw2-1-solution

--- src/main/java/core/basesyntax/FlipBit.java
+++ src/main/java/core/basesyntax/FlipBit.java
@@ -9,6 +9,7 @@ public class FlipBit {
      * Договоримся, что биты нумеруются от младшего (индекс 1) к старшему (индекс 32).</p>
      */
     public int flipBit(int value, int bitIndex) {
-        return 0;
+        int temp = (1 << (bitIndex - 1));
+        return (value ^ temp);
     }
 }
