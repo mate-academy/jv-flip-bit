16d7e3066bb8119e61a63f2473eb3994cd1a733c
Polina Kulyk <polina.kulyk@protonmail.com>
2020-03-31 13:04:46 +0300

    eliminated comments

--- src/main/java/core/basesyntax/FlipBit.java
+++ src/main/java/core/basesyntax/FlipBit.java
@@ -9,9 +9,6 @@ public class FlipBit {
      * Договоримся, что биты нумеруются от младшего (индекс 1) к старшему (индекс 32).</p>
      */
     public int flipBit(int value, int bitIndex) {
-        // reach the bit at the given index by shifting bits
-        // create mask of the number where only predefined bit is set to 1
-        // using XOR replace that bit (XOR returns "true" only if there is one true)
         return value ^ (1 << (bitIndex - 1));
     }
 }
