20212814727cbca9be78288288235ad111f7ec24
NikolayAbysov <nikolay.abysov@ukr.net>
2020-03-31 13:44:35 +0300

    Solution implemented

--- src/main/java/core/basesyntax/FlipBit.java
+++ src/main/java/core/basesyntax/FlipBit.java
@@ -8,7 +8,23 @@ public class FlipBit {
      * Данная задача актуальна, например, при работе с битовыми полями.
      * Договоримся, что биты нумеруются от младшего (индекс 1) к старшему (индекс 32).</p>
      */
-    public int flipBit(int value, int bitIndex) {
-        return 0;
+    public static int flipBit(int value, int bitIndex) {
+        bitIndex = bitIndex - 1;
+        StringBuilder stringBuilder = new StringBuilder(Integer.toBinaryString(value));
+        stringBuilder = stringBuilder.reverse();
+
+        if (stringBuilder.length() > bitIndex) {
+            if (stringBuilder.charAt(bitIndex) == '1') {
+                stringBuilder.setCharAt(bitIndex, '0');
+            } else {
+                stringBuilder.setCharAt(bitIndex, '1');
+            }
+        } else {
+            while (stringBuilder.length() <= bitIndex - 1) {
+                stringBuilder.append('0');
+            }
+            stringBuilder.append('1');
+        }
+        return Integer.parseInt(stringBuilder.reverse().toString(), 2);
     }
 }
