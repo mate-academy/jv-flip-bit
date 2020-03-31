c93469180d21e405781782acdf0d9403b70762da
Serhii Tsyhankov <40259977+fruts@users.noreply.github.com>
2020-03-31 13:40:38 +0300

    edit to bitwise

--- src/main/java/core/basesyntax/FlipBit.java
+++ src/main/java/core/basesyntax/FlipBit.java
@@ -9,25 +9,6 @@ public class FlipBit {
      * Договоримся, что биты нумеруются от младшего (индекс 1) к старшему (индекс 32).</p>
      */
     public static int flipBit(int value, int bitIndex) {
-        int[] bitArray = new int[32];
-        String bitString = Integer.toBinaryString(value);
-        char[] tempBitArray = bitString.toCharArray();
-        int j = 0;
-        String res = "";
-        for (int i = bitArray.length - tempBitArray.length; i < bitArray.length; i++) {
-            String x = "";
-            x += tempBitArray[j];
-            bitArray[i] = Integer.parseInt(x);
-            j++;
-        }
-        if (bitArray[bitArray.length - bitIndex] == 0) {
-            bitArray[bitArray.length - bitIndex] = 1;
-        } else {
-            bitArray[bitArray.length - bitIndex] = 0;
-        }
-        for (int i = 0; i < bitArray.length; i++) {
-            res += bitArray[i];
-        }
-        return Integer.parseInt(res, 2);
+        return value ^ (1 << bitIndex - 1);
     }
 }
