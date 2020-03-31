3a9c2ee3d3a3a831098409e9a7b5cba1200566f0
ihor <krohmal.igor777@gmail.com>
2020-03-31 14:12:51 +0300

    changed

--- src/main/java/core/basesyntax/FlipBit.java
+++ src/main/java/core/basesyntax/FlipBit.java
@@ -9,20 +9,6 @@ public class FlipBit {
      * Договоримся, что биты нумеруются от младшего (индекс 1) к старшему (индекс 32).</p>
      */
     public int flipBit(int value, int bitIndex) {
-        StringBuilder stringBuilderBinaryValue = new StringBuilder(Integer.toBinaryString(value));
-        stringBuilderBinaryValue.reverse();
-        while (stringBuilderBinaryValue.length() < 32) {
-            stringBuilderBinaryValue.append('0');
-        }
-        stringBuilderBinaryValue.reverse();
-        int indexChangeBite = stringBuilderBinaryValue.length() - bitIndex;
-        if (stringBuilderBinaryValue.charAt(indexChangeBite) == '1') {
-            stringBuilderBinaryValue.replace(indexChangeBite,
-                    indexChangeBite + 1, String.valueOf('0'));
-        } else {
-            stringBuilderBinaryValue.replace(indexChangeBite,
-                    indexChangeBite + 1, String.valueOf('1'));
-        }
-        return Integer.parseInt(stringBuilderBinaryValue.toString(), 2);
+        return value ^ (1 << bitIndex - 1);
     }
 }
