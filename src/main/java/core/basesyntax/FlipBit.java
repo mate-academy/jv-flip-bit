c254ad69dcc9c262a7ba19acb4389ac6d25da7ef
Kseniia Makarova <kseniia.makarova.kyiv@gmail.com>
2020-03-31 14:26:04 +0300

    refactored the solution

--- src/main/java/core/basesyntax/FlipBit.java
+++ src/main/java/core/basesyntax/FlipBit.java
@@ -9,7 +9,6 @@ public class FlipBit {
      * Договоримся, что биты нумеруются от младшего (индекс 1) к старшему (индекс 32).</p>
      */
     public int flipBit(int value, int bitIndex) {
-        int mask = 1 << bitIndex - 1;
-        return (value & mask) != 0 ? value & (~ mask) : value | mask;
+        return value ^ 1 << bitIndex - 1;
     }
 }
